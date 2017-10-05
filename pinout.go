9fc3b11913f114e50ae58607e29872d4ae038d5e
Frank Moreno <frankmoreno1993@gmail.com>
2017-10-05 14:49:16 -0500

    Adding easier pinout names

---
+++ pinout.go
@@ -0,0 +1,77 @@
+package rpio
+
+const (
+  // Phys1: 3.3V
+  // Phys2: 5V
+  Phys3 uint8 = 0
+  // Phys4: 5V
+  Phys5 uint8 = 1
+  // Phys6: Ground
+  Phys7 uint8 = 4
+  Phys8 uint8 = 14
+  // Phys9: Ground
+  Phys10 uint8 = 15
+  Phys11 uint8 = 17
+  Phys12 uint8 = 18
+  Phys13 uint8 = 21
+  // Phys14: Ground
+  Phys15 uint8 = 22
+  Phys16 uint8 = 23
+  // Phys17: 3.3V
+  Phys18 uint8 = 24
+  Phys19 uint8 = 10
+  // Phys20: Ground
+  Phys21 uint8 = 9
+  Phys22 uint8 = 25
+  Phys23 uint8 = 11
+  Phys24 uint8 = 8
+  // Phys25: Ground
+  Phys26 uint8 = 7
+  /* Do not use yet the following pins*/
+  Phys27 uint8 = 0
+  Phys28 uint8 = 0
+  Phys29 uint8 = 0
+  Phys30 uint8 = 0
+  Phys31 uint8 = 0
+  Phys32 uint8 = 0
+  Phys33 uint8 = 0
+  Phys34 uint8 = 0
+  Phys35 uint8 = 0
+  Phys36 uint8 = 0
+  Phys37 uint8 = 0
+  Phys38 uint8 = 0
+  Phys39 uint8 = 0
+  Phys40 uint8 = 0
+)
+
+
+const (
+  BCM0  uint8 = Phys27
+  BCM1  uint8 = Phys28
+  BCM2  uint8 = Phys3
+  BCM3  uint8 = Phys5
+  BCM4  uint8 = Phys7
+  BCM5  uint8 = Phys29
+  BCM6  uint8 = Phys37
+  BCM7  uint8 = Phys26
+  BCM8  uint8 = Phys24
+  BCM9  uint8 = Phys21
+  BCM10 uint8 = Phys19
+  BCM11 uint8 = Phys23
+  BCM12 uint8 = Phys32
+  BCM13 uint8 = Phys32
+  BCM14 uint8 = Phys8
+  BCM15 uint8 = Phys10
+  BCM16 uint8 = Phys36
+  BCM17 uint8 = Phys11
+  BCM18 uint8 = Phys12
+  BCM19 uint8 = Phys35
+  BCM20 uint8 = Phys38
+  BCM21 uint8 = Phys40
+  BCM22 uint8 = Phys15
+  BCM23 uint8 = Phys16
+  BCM24 uint8 = Phys18
+  BCM25 uint8 = Phys22
+  BCM26 uint8 = Phys37
+  BCM27 uint8 = Phys13
+)
