56e413cf95a1479a45d695750b59bac836f7dca8
Frank Moreno <frankmoreno1993@gmail.com>
2017-10-05 19:12:28 -0500

    pinout was only for rev1

--- rev1/pinout.go
+++ rev1/pinout.go
@@ -1,4 +1,4 @@
-package rpio
+package rev1
 
 const (
   // Phys1: 3.3V
@@ -27,51 +27,26 @@ const (
   Phys24 uint8 = 8
   // Phys25: Ground
   Phys26 uint8 = 7
-  /* Do not use yet the following pins*/
-  Phys27 uint8 = 0
-  Phys28 uint8 = 0
-  Phys29 uint8 = 0
-  Phys30 uint8 = 0
-  Phys31 uint8 = 0
-  Phys32 uint8 = 0
-  Phys33 uint8 = 0
-  Phys34 uint8 = 0
-  Phys35 uint8 = 0
-  Phys36 uint8 = 0
-  Phys37 uint8 = 0
-  Phys38 uint8 = 0
-  Phys39 uint8 = 0
-  Phys40 uint8 = 0
 )
 
 
 const (
-  BCM0  uint8 = Phys27
-  BCM1  uint8 = Phys28
   BCM2  uint8 = Phys3
   BCM3  uint8 = Phys5
   BCM4  uint8 = Phys7
-  BCM5  uint8 = Phys29
   BCM6  uint8 = Phys37
   BCM7  uint8 = Phys26
   BCM8  uint8 = Phys24
   BCM9  uint8 = Phys21
   BCM10 uint8 = Phys19
   BCM11 uint8 = Phys23
-  BCM12 uint8 = Phys32
-  BCM13 uint8 = Phys32
   BCM14 uint8 = Phys8
   BCM15 uint8 = Phys10
-  BCM16 uint8 = Phys36
   BCM17 uint8 = Phys11
   BCM18 uint8 = Phys12
-  BCM19 uint8 = Phys35
-  BCM20 uint8 = Phys38
-  BCM21 uint8 = Phys40
   BCM22 uint8 = Phys15
   BCM23 uint8 = Phys16
   BCM24 uint8 = Phys18
   BCM25 uint8 = Phys22
-  BCM26 uint8 = Phys37
   BCM27 uint8 = Phys13
 )
